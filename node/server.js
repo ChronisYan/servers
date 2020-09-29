85941545e099e48a7424fac516a1c4299925224f
ChronisG <thegamedevelopergame@gmail.com>
2020-09-29 13:37:03 +0100

    node server

---
+++ node/server.js
@@ -0,0 +1,39 @@
+const express = require("express");
+const helmet = require("helmet");
+const MongoClient = require("mongodb").MongoClient;
+const path = require("path");
+const bodyParser = require("body-parser");
+
+// express setup
+const PORT = 4000;
+const app = express();
+app.use(helmet());
+app.use(bodyParser.json());
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+
+// Routes
+app.get("/", (req, res) => {
+  res.send({ msg: "Welcome!" });
+});
+
+// mongoDB setup
+const URL = "mongodb://127.0.0.1:27017";
+const DB_NAME = "test";
+let quotes = [];
+let topQuote = "";
+MongoClient.connect(
+  URL,
+  { useNewUrlParser: true, useUnifiedTopology: true },
+  (err, client) => {
+    if (err) return console.log("Unable to connect to the Database");
+
+    // load quotes
+    quotes = client.db(DB_NAME).collection("quotes");
+    // load top quote
+
+    app.listen(PORT, () => {
+      console.log(`Server running on port ${PORT}`);
+    });
+  }
+);
